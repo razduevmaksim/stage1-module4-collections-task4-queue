bedcdee4d03d1ad79c5fc68e81eaf2dea4f3ad5e
Maksim <mrazduev@gmail.com>
2024-12-25 16:59:50 +0300

    fix

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,13 +1,29 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public static ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
-        ArrayDeque<Integer> finalDeque = new ArrayDeque<>();
+        ArrayDeque<Integer> finalQueue = new ArrayDeque<>();
 
-        return finalDeque;
+        finalQueue.offer(Objects.requireNonNull(firstQueue.poll()));
+        finalQueue.offer(Objects.requireNonNull(firstQueue.poll()));
+        finalQueue.offer(Objects.requireNonNull(secondQueue.poll()));
+        finalQueue.offer(Objects.requireNonNull(secondQueue.poll()));
+
+        while ((!firstQueue.isEmpty() && !secondQueue.isEmpty())) {
+            firstQueue.offer(finalQueue.pollLast());
+            finalQueue.offer(Objects.requireNonNull(firstQueue.poll()));
+            finalQueue.offer(Objects.requireNonNull(firstQueue.poll()));
+
+            secondQueue.offer(finalQueue.pollLast());
+            finalQueue.offer(Objects.requireNonNull(secondQueue.poll()));
+            finalQueue.offer(Objects.requireNonNull(secondQueue.poll()));
+        }
+
+        return finalQueue;
     }
 }
